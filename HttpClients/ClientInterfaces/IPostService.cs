20358f55e3a401dafcb3bd06081a4286af3d6e5b
Bjarke <bjarkebp@live.dk>
2022-11-03 15:34:20 +0100

    Hej

--- HttpClients/ClientInterfaces/IPostService.cs
+++ HttpClients/ClientInterfaces/IPostService.cs
@@ -7,11 +7,7 @@ public interface IPostService
 {
     Task CreateAsync(PostCreationDto dto);
     
-    Task<ICollection<Post>> GetAsync(
-        string? Title, 
-        string? Body, 
-        int? postId
-    );
+    Task<ICollection<Post>> GetAsync(string? Title, string? Body, int? postId);
     
     Task<Post> GetByIdAsync(int id);
 }
